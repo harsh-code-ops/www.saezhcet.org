f9abc6fd4e1e1787d9148de898cf78b92dc6f42f
harsh-code-ops <rajaharsh751@gmail.com>
2025-08-19 07:49:17 +0530

    Governig_board

--- src/Pages/Components/Aboutus/Governingboarddata.js
+++ src/Pages/Components/Aboutus/Governingboarddata.js
@@ -1,52 +1,70 @@
 export default [
   {
-    Name: "Mohd Munazir Mairaj",
+    Name: "Mohammad Mudassir Ali",
     Post: "Chairperson",
-    LinkedIn: "https://www.linkedin.com/in/mohd-munazir-mairaj-1432761b1",
-    Insta: "https://www.instagram.com/i__am___munazir/",
-    Email: "munazirmairaj8976@gmail.com",
-    Img: "mohd_munazir.jpg",
-  },
-
-  {
-    Name: "Nirmanyu Goel",
-    Post: "Vice-Chairperson",
-    LinkedIn: "",
-    Insta: "https://www.instagram.com/_nirmanyu_goel/",
-    Email: "nirmanyugoel@gmail.com",
-    Img: "Nirmanyu_Goel.jpg",
-  },
-
-  {
-    Name: "Ayush Gupta",
-    Post: "Secretary",
-    LinkedIn: "https://in.linkedin.com/in/ayush-gupta-3a93621b6",
-    Insta: "https://www.instagram.com/laserx_1729/",
-    Email: "ayush150152@gmail.com",
-    Img: "ayush_gupta.jpg",
-  },
-
-  {
-    Name: "Saif Shakeel",
-    Post: "Secretary",
     LinkedIn: "",
     Insta: "",
-    Email: "saifshakeel2001@gmail.com",
-    Img: "saif.jpg",
+    Email: "",
+    Img: "mudassir.jpg",
   },
 
   {
     Name: "Syed Nabeel Hasan",
-    Post: "Coordinator: Membership Recruitment & Workshop Commitee",
+    Post: "Vice-Chairperson",
     LinkedIn:
       "https://www.linkedin.com/in/nabeel-hasan-32381a289?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     Insta: "",
     Email: "nabeelhasan661@gmail.com",
     Img: "nabeel_hasan.jpeg",
   },
+
+  {
+    Name: "Mohammad Haris",
+    Post: "Secretary",
+    LinkedIn: "",
+    Insta: "",
+    Email: "",
+    Img: "Haris.png",
+  },
+
+  {
+    Name: "Batte Arafat Degea",
+    Post: "Secretary",
+    LinkedIn: "",
+    Insta: "",
+    Email: "",
+    Img: "batte.jpg",
+  },
+
+  {
+    Name: "Adnan Hussain",
+    Post: "Treasurer",
+    LinkedIn: "",
+    Insta: "",
+    Email: "",
+    Img: "",
+  },
+
+  {
+    Name: "Injila Parwez",
+    Post: "Alumni-Relations Coordinator",
+    LinkedIn: "",
+    Insta: "",
+    Email: "",
+    Img: "injila.jpeg",
+  },
+
+  {
+    Name: "Mustafa Rais",
+    Post: "Recruitment and Workshop Coordinator",
+    LinkedIn: "",
+    Insta: "",
+    Email: "",
+    Img: "mustafa.jpeg",
+  },
   {
     Name: "Yashra Malik",
-    Post: "Coordinator: Public Relations Committee",
+    Post: "Public Relations and Outreach Cordinator",
     LinkedIn:
       "https://www.linkedin.com/in/yashra-malik-77313528b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     Insta: "",
@@ -55,50 +73,50 @@ export default [
   },
 
   {
-    Name: "Abdus Samad",
-    Post: "Coordinator: Editorial Board",
-    LinkedIn: "https://in.linkedin.com/in/abdus-samad-945249225",
-    Insta: "",
-    Email: "abdussamad8077@gmail.com",
-    Img: "abdus_samad.jpeg",
-  },
-
-  {
-    Name: "Mohammad Mudassir Ali",
-    Post: "Coordinator: Alumni Relations and Industry Interaction Committee",
+    Name: "Khadija Fatima Abbasi",
+    Post: "Asst. Public Relations and Outreach Cordinator",
     LinkedIn: "",
     Insta: "",
     Email: "",
-    Img: "mudassir.jpg",
-  },
-
-  {
-    Name: "Md Shafatullah",
-    Post: "Executive Member",
-    LinkedIn: "",
-    Insta: "",
-    Email: "",
-    Img: "shafatullah.jpeg",
-  },
-
-  {
-    Name: "Ibrahim Ahmed",
-    Post: "Executive Member",
-    LinkedIn: "https://www.linkedin.com/in/ibrahim-ahmed-61784b297",
-    Insta: "",
-    Email: "ibrahimahmedlko@gmail.com",
-    Img: "Ibrahim_ahmad.png",
+    Img: "khadija.jpg",
   },
 
   {
     Name: "Harsh Raja",
-    Post: "Web Developer Head",
+    Post: "Web Developer",
     LinkedIn:
       "https://www.linkedin.com/in/harsh-raja-8763a0290?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     Insta:
       "https://www.instagram.com/only.harxh/profilecard/?igsh=Z2xpeWNmejNpbzZx",
     Email: "rajaharsh751@gmail.com",
     Img: "Harsh_raja.png",
+  },
+
+  {
+    Name: "Needa Khan",
+    Post: "Assistant Web Developer",
+    LinkedIn: "https://www.linkedin.com/in/needa-khan/",
+    Insta: "",
+    Email: "needak0205@gmail.com",
+    Img: "needa.jpeg",
+  },
+
+  {
+    Name: "Mukhtar Khan",
+    Post: "Executive Member",
+    LinkedIn: "",
+    Insta: "",
+    Email: "",
+    Img: "mukhtar.jpeg",
+  },
+
+  {
+    Name: "Mohammad Faheem Abidi",
+    Post: "Executive Member",
+    LinkedIn: "",
+    Insta: "",
+    Email: "",
+    Img: "faheem.png",
   },
 
   // {
